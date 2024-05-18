b8eb7935cd9d1dc7a884ad56b7622e400a58675e
Iorsharck <56987309+Iorsharck@users.noreply.github.com>
2024-05-17 19:46:11 -0600

    Update Menu2.ps1
    Termiando el menu

--- Menu2.ps1
+++ Menu2.ps1
@@ -1,10 +1,10 @@
-# Personalizo el saludo según el horario
+# Personalizo el saludo segÃºn el horario
 function ObtenerSaludo {
     $horaActual = Get-Date
     $hora = $horaActual.Hour
 
     if ($hora -ge 5 -and $hora -lt 12) {
-        return "Buenos días"
+        return "Buenos dÃ­as"
     } elseif ($hora -ge 12 -and $hora -lt 18) {
         return "Buenas tardes"
     } else {
@@ -12,26 +12,24 @@ function ObtenerSaludo {
     }
 }
 
-# Menú
-function MostrarMenu {
+# Menu
+function MainMenu {
     @"
 =====================
-||      Menú:      ||
+||      Menu:      ||
 =====================
-(1) Argparse (con también el help)
-(2) Escaneo de puertos
-(3) Imagen API
-(4) Imagen Fernet
-(5) Metadatos de la Imagen
-(6) Trasmisión
-(7) Webscrapping
-(8) 3 tareas se conecten
-(9) Salir
+(1) Descargar imagen random, limpia metadatos, encripta el mensaje en la imagen
+(2) Desencriptar mensaje y crear reporte
+(3) Escaneo de puertos
+(4) Obtener imagenes de una web
+(5) Checar si API es maliciosa
+(6) Salir
 =====================
 "@
 }
 
-# Función para ejecutar el script de Python con argparse
+
+# FunciÃ³n para ejecutar el script de Python con argparse
 function EjecutarScriptConArgparse {
     param (
         [string]$rutaScript,
@@ -46,10 +44,10 @@ function EsperarDosSegundos {
     Start-Sleep -Seconds 2
 }
 
-# Muestra el mensaje según el horario
+# Muestra el mensaje segÃºn el horario
 function MostrarMensajeDeHorario {
     $saludo = ObtenerSaludo
-    Write-Host $saludo ", ¿Qué deseas hacer hoy pinponero?"
+    Write-Host $saludo ", Que deseas hacer hoy pinponero?"
 }
 
 # Mostrar el mensaje de acuerdo al horario
@@ -58,49 +56,63 @@ MostrarMensajeDeHorario
 # Esperar 2 segundos
 EsperarDosSegundos
 
-# Mostrar el menú
-MostrarMenu
+# Mostrar el menÃº
+MainMenu
 
 # Preguntar al pinponero que va a hacer
-$opcion = Read-Host "Seleccione una opción del menú (1-9)"
+
 
 # Arrays/Lista con los nombres de los programas
-$rutaCodigos = @("ScriptConArgparse","escaneopuertos.py","imagenAPI.py","imagenFernet.py","metadatos_imag.py","trasmision.py","webscrappy.py")
+$rutaCodigos = @("imagenAPI.py","imagenFernet.py","transmision.py","escaneopuertos.py","webscrappy.py","ConsultaDeApis.py")
 
-# Ejecutar el script correspondiente a la opción seleccionada
-switch ($opcion) {
-    "1" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
+# Ejecutar el script correspondiente a la opciÃ³n seleccionada
+while ($true)
+{
+    $argsPython = @()
+    $opcion = Read-Host "Seleccione una opcion del menu (1-6)"
+    switch ($opcion) {
+        "1" {
+            $imagen = EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0])
+            $mg = Read-Host "Escriba el mensaje que se encriptara"
+            $argsPython = "-m","encriptar","-p",$imagen,"-me", $mg
+            $e = EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[1]) -argsPython $argsPython
+        }
+        "2" {
+            $argsPython = "-m","desencriptar","-p",$imagen, "-me", $e
+            echo "Este es el codigo decifrado:" @(EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[1]) -argsPython $argsPython)
+        }
+        "3" {#
+            $ip = Read-Host "Escriba una ip para scannear puertos populares"
+            if ($web -eq ""){
+            $argsPython = "--host","2.2.2.2"
+            EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[3])
+            }else{
+            $argsPython = "--host",$ip
+            EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[3]) -argsPython $argsPython
+        }
+        }
+        "4" {
+            $web = Read-Host "Escriba una pagina para verificar si es segura, dejalo en blanco para poner web por default"
+            if ($web -eq ""){
+            EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[4])
+            }else{
+            $argsPython = "--web",$web
+            EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[4]) -argsPython $argsPython
+            }
+        }
+        
+        "5" {
+            $ip = Read-Host "Escriba una ip para saber si es segura"
+            $argsPython = "--ip",$ip
+            if ($ip -eq ""){
+            $argsPython = "--ip","1.1.1.1"
+            EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[5])
+            }else{
+
+            EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[5]) -argsPython $argsPython
+            }
+        }
+        "6" {exit}
+        Default { Write-Host "Opción no válida, por favor elija otra opcion." }
     }
-    "2" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
     }
-    "3" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
-    }
-    "4" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
-    }
-    "5" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
-    }
-    "6" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
-    }
-    "7" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
-    }
-    "8" {
-        $argsPython = "--opcion1","valor1","--opcion2","valor2","--opcion3","valor3","--opcion4","valor4","--opcion5","valor5","--opcion6","valor6","--opcion7","valor7","--opcion8","valor8"
-        EjecutarScriptConArgparse -rutaScript @(Join-Path -Path $PWD -ChildPath $rutaCodigos[0]) -argsPython $argsPython
-    }
-    "9" {exit}
-    Default { Write-Host "Opción no válida, por favor elija otra opción." }
-}
